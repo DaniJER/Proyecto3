fec6162c4e2561dad5025dcd4010b4fb69e5014c
Jv-2011 <juannoviembre20@gmail.com>
2023-11-18 20:21:58 -0500

    Arregle errores de la vista

--- src/main/java/view/ListView.java
+++ src/main/java/view/ListView.java
@@ -26,21 +26,100 @@ public class ListView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jTextField17 = new javax.swing.JTextField();
+        botonEliminar = new javax.swing.JButton();
+        buscarId = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        botonVolver = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(196, 196, 196));
+        jPanel1.setLayout(null);
+
+        jTextField17.setBackground(new java.awt.Color(196, 196, 196));
+        jTextField17.setFont(new java.awt.Font("Arial", 1, 16)); // NOI18N
+        jTextField17.setForeground(new java.awt.Color(0, 0, 0));
+        jTextField17.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField17.setText("Developed by jviedman dev - enriquez dev");
+        jTextField17.setBorder(null);
+        jPanel1.add(jTextField17);
+        jTextField17.setBounds(211, 540, 520, 19);
+
+        botonEliminar.setBackground(new java.awt.Color(131, 131, 131));
+        botonEliminar.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        botonEliminar.setForeground(new java.awt.Color(0, 0, 0));
+        botonEliminar.setText("Aceptar");
+        botonEliminar.setBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED, java.awt.Color.black, java.awt.Color.black));
+        botonEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonEliminarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(botonEliminar);
+        botonEliminar.setBounds(410, 400, 170, 60);
+
+        buscarId.setBackground(new java.awt.Color(255, 255, 255));
+        buscarId.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
+        jPanel1.add(buscarId);
+        buscarId.setBounds(330, 280, 320, 65);
+
+        jLabel5.setBackground(new java.awt.Color(196, 196, 196));
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel5.setText("<html>Digite el número de cédula del usuario qué desea consultar</html> ");
+        jLabel5.setFocusable(false);
+        jLabel5.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        jLabel5.setMaximumSize(new java.awt.Dimension(0, 0));
+        jLabel5.setMinimumSize(new java.awt.Dimension(0, 0));
+        jLabel5.setPreferredSize(new java.awt.Dimension(0, 0));
+        jLabel5.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(190, 150, 690, 70);
+
+        botonVolver.setBackground(new java.awt.Color(196, 196, 196));
+        botonVolver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/close.png"))); // NOI18N
+        botonVolver.setBorder(null);
+        botonVolver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonVolverActionPerformed(evt);
+            }
+        });
+        jPanel1.add(botonVolver);
+        botonVolver.setBounds(60, 50, 100, 90);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 950, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 950, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEliminarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botonEliminarActionPerformed
+
+    private void botonVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonVolverActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botonVolverActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +157,11 @@ public class ListView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton botonEliminar;
+    public javax.swing.JButton botonVolver;
+    public javax.swing.JTextField buscarId;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField17;
     // End of variables declaration//GEN-END:variables
 }
