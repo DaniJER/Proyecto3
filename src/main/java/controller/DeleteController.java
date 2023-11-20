cab01671c4cded9a7eb0351d636f0c2697eedc6b
Daniel Enriquez <danicraft.op7@gmail.com>
2023-11-20 13:09:25 -0500

    guardando cambios en develop

---
+++ src/main/java/controller/DeleteController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view. *;
+import model. *;
+
+/**
+ *
+ * @author Juan
+ */
+public class DeleteController implements ActionListener{
+    private PrincipalView vistaPrincipal;
+    private DeleteView vistaEliminar;
+
+    public DeleteController(PrincipalView vistaPrincipal, DeleteView vistaEliminar) {
+        this.vistaPrincipal = vistaPrincipal;
+        this.vistaEliminar = vistaEliminar;
+        vistaPrincipal.botonEliminar.addActionListener(this);
+        vistaEliminar.botonEliminar.addActionListener(this);
+        vistaEliminar.botonVolver.addActionListener(this);
+    }
+    
+    public void iniciarVistaEliminar (){
+        vistaEliminar.setTitle("Univalle");
+        vistaEliminar.setLocationRelativeTo (null);
+        vistaEliminar.setSize(vistaPrincipal.getWidth(), vistaPrincipal.getHeight());
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource () == vistaPrincipal.botonEliminar){
+            vistaPrincipal.dispose();
+            vistaEliminar.setVisible(true);
+            
+        }
+        
+        if(e.getSource() == vistaEliminar.botonVolver){
+            vistaEliminar.dispose();
+            vistaPrincipal.setVisible(true);
+        }
+    } 
+    
+}
---
+++ src/main/java/model/Read.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;  
+
+/**
+ *
+ * @author Juan
+ */
+public class Read {
+    public void leerColeccion(){
+        
+        Create coleccion = new Create();
+        System.out.println(coleccion.getId());
+        }
+}
